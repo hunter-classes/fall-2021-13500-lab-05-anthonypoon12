3fcd0f2695e900d8b3bd407c006dc56d81f656b9
anthonypoon12 <anthony@poons.us>
2021-09-30 09:47:19 -0400

    Added test cases for isDivisibleBy

--- tests.cpp
+++ tests.cpp
@@ -2,4 +2,8 @@
 #include "doctest.h"
 #include "funcs.h"
 
-// add your tests here
+CHECK(isDivisibleBy(10,5)==true);
+CHECK(isDivisibleBy(10,0)==false);
+CHECK(isDivisibleBy(10,50)==true);
+CHECK(isDivisibleBy(1,5)==false);
+CHECK(isDivisibleBy(0,5)==false);
