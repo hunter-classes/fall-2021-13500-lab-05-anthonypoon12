ab0efb3fce1505c0c0d907f87c4113fd18cc44ca
anthonypoon12 <anthony@poons.us>
2021-09-30 09:51:21 -0400

    Edit test cases for isDivisibleBy

--- tests.cpp
+++ tests.cpp
@@ -2,8 +2,14 @@
 #include "doctest.h"
 #include "funcs.h"
 
-CHECK(isDivisibleBy(10,5)==true);
-CHECK(isDivisibleBy(10,0)==false);
-CHECK(isDivisibleBy(10,50)==true);
-CHECK(isDivisibleBy(1,5)==false);
-CHECK(isDivisibleBy(0,5)==false);
+TEST_CASE("isDivisibleBy normal cases")
+{
+    CHECK(isDivisibleBy(10,5)==true);
+    CHECK(isDivisibleBy(1,5)==false);
+    CHECK(isDivisibleBy(10,50)==true);
+}
+TEST_CASE("isDivisibleBy cases with 0")
+{
+    CHECK(isDivisibleBy(10,0)==false);
+    CHECK(isDivisibleBy(0,5)==false);
+}  
