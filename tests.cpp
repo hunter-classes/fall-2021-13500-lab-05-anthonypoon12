25df2dd64d3a52148ecfadf192b40c8dc957c5f5
anthonypoon12 <anthony@poons.us>
2021-09-30 10:03:56 -0400

    added test cases for nextPrime

--- tests.cpp
+++ tests.cpp
@@ -27,3 +27,25 @@ TEST_CASE("isPrime normal cases")
     CHECK(isPrime(121)==true);
     CHECK(isPrime(97)==true);
 }
+TEST_CASE("nextPrime cases for 0, 1, and 2")
+{
+    CHECK(nextPrime(0)==2);
+    CHECK(nextPrime(1)==2);
+}
+TEST_CASE("nextPrime cases for primes")
+{
+    CHECK(nextPrime(2)==3);
+    CHECK(nextPrime(3)==5);
+    CHECK(nextPrime(5)==7);
+    CHECK(nextPrime(11)==13);
+}
+TEST_CASE("nextPrime normal cases")
+{
+    CHECK(nextPrime(21)==23);
+    CHECK(nextPrime(26)==29);
+    CHECK(nextPrime(4)==5);
+TEST_CASE("nextPrime negative cases")
+{
+    CHECK(nextPrime(-1)==2);
+    CHECK(nextPrime(=100)==2);
+}
